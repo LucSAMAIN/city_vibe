d1958cba2ef1547057901ecffb2a2ec9394a3687
remivCode <remi.vialleton@gmail.com>
2025-12-12 17:13:30 +0100

    feat: clean values

--- dags/staging_dag.py
+++ dags/staging_dag.py
@@ -46,33 +46,13 @@ def clean_column(name):
     # Lowercase
     return name.lower()
 
-def clean_value(v):
-    # Convert Mongo ObjectId or any object → string
-    if not isinstance(v, (int, float, str)) and v is not None:
-        v = str(v)
-
-    # NULL handling
-    if v is None:
-        return "NULL"
-
-    # Numeric values → keep raw
-    if isinstance(v, (int, float)):
-        return str(v)
-
-    # Strings → escape single quotes
-    if isinstance(v, str):
-        v = v.replace("'", "''")
-        return f"'{v}'"
-
-    # Fallback
-    return f"'{str(v)}'"
-
 ## Task functions
 
 def _mongo_to_postgres():
     import psycopg2
     from pymongo import MongoClient
     import pandas as pd
+    import numpy as np
     import io
 
     # Connect to Mongo
@@ -124,14 +104,37 @@ def _mongo_to_postgres():
         col_list_sql = ", ".join([f'"{c}"' for c in clean_cols if c in COLUMN_TYPE_MAPPING.keys()] + ['"date"'])
 
         # COPY buffer reusable object
-        def copy_rows(row_batch):
+        def copy_rows(batch: pd.DataFrame):
             csv_buffer = io.StringIO()
-            for row in row_batch:
-                cleaned_row = [clean_value(row.get(orig_col)) for orig_col in df.columns]
-                csv_buffer.write(",".join(cleaned_row) + "\n")
+            
+            batch["nombre_de_foyers_fiscaux"] = pd.to_numeric(batch["nombre_de_foyers_fiscaux"], errors='coerce', downcast='integer')
+            batch["nombre_de_foyers_fiscaux"] = batch["nombre_de_foyers_fiscaux"].astype('Int64')
+            batch["revenu_fiscal_de_reference_des_foyers_fiscaux"] = pd.to_numeric(batch["revenu_fiscal_de_reference_des_foyers_fiscaux"], errors='coerce', downcast='float')
+            batch["impot_net_total"] = pd.to_numeric(batch["impot_net_total"], errors='coerce', downcast='float')
+            batch["nombre_de_foyers_fiscaux_imposes"] = pd.to_numeric(batch["nombre_de_foyers_fiscaux_imposes"], errors='coerce', downcast='integer')
+            batch["nombre_de_foyers_fiscaux_imposes"] = batch["nombre_de_foyers_fiscaux_imposes"].astype('Int64')
+            batch["revenu_fiscal_de_reference_des_foyers_fiscaux_imposes"] = pd.to_numeric(batch["revenu_fiscal_de_reference_des_foyers_fiscaux_imposes"], errors='coerce', downcast='float')
+            
+            def safe_str(val):
+                if pd.isna(val) or val == "":
+                    return None
+                return str(val)
+                
+            batch['dep'] = batch['dep'].apply(safe_str)
+            batch['commune'] = batch['commune'].apply(lambda x: int(x)).apply(safe_str)
+            batch['libelle_de_la_commune'] = batch['libelle_de_la_commune'].apply(safe_str)
+            batch['revenu_fiscal_de_reference_par_tranche_en_euros'] = batch['revenu_fiscal_de_reference_par_tranche_en_euros'].apply(safe_str).apply(lambda x: x.upper().strip() if x is not None else x)
+            batch['date'] = int(collection.split("_")[1])
+
+            batch = batch[batch['revenu_fiscal_de_reference_par_tranche_en_euros'] == 'TOTAL']
+
+            batch = batch.replace({np.nan: None})
+
+            batch = batch[[c for c in clean_cols if c in COLUMN_TYPE_MAPPING.keys()] + ['date']]
+
+            batch.to_csv(csv_buffer, index=False, header=False, na_rep='')
 
             csv_buffer.seek(0)
-
             cur.copy_expert(
                 f'COPY REVENUE ({col_list_sql}) FROM STDIN WITH CSV',
                 csv_buffer
@@ -139,7 +142,7 @@ def _mongo_to_postgres():
             conn.commit()
 
         # Send first batch
-        copy_rows(first_batch)
+        copy_rows(df)
 
         # Stream remaining batches
         batch = []
@@ -147,12 +150,12 @@ def _mongo_to_postgres():
             batch.append(doc)
 
             if len(batch) >= 2000:
-                copy_rows(batch)
+                copy_rows(pd.DataFrame(batch))
                 batch = []
 
         # Last incomplete batch
         if batch:
-            copy_rows(batch)
+            copy_rows(pd.DataFrame(batch))
 
 ## Staging DAG definition
 
